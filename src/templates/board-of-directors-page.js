9a3093f52ddb6ce3ce6a9f26a23e7b6ce48715de
mmadde10 <mmadden1495@gmail.co>
2022-04-26 18:17:47 -0500

    whoops

--- src/templates/board-of-directors-page.js
+++ src/templates/board-of-directors-page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "../layouts/Main";
 import { Typography } from "@mui/material";
+import { graphql } from "gatsby";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
